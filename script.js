e6ab32e609552ee92ed3bc1761e96b2a5c291bc7
sornpetcht <sornpetcht@local.x>
2026-07-07 10:41:08 +0700

    add feature2

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const button = document.getElementById("greetBtn");
-const message = document.getElementById("message");
-
-if (button && message) {
-  button.addEventListener("click", () => {
-    message.textContent = "สวัสดี! หน้าเว็บของคุณพร้อมใช้งานแล้ว";
-  });
-}
+console.log("Hello Mama");
